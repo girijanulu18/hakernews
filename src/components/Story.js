d234cc1e228f5dd73530b95b9b4e36c4c0d9e743
Rajeshanupoju3 <techstackdeveloper@gmail.com>
2022-10-15 22:14:28 +0530

    assessment

---
+++ src/components/Story.js
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, memo } from 'react';
+import { getStory } from '../services/hnApi';
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement,
+} from '../styles/StoryStyles';
+
+export const Story = memo(function Story({ storyId }) {
+  const [story, setStory] = useState({});
+
+  useEffect(() => {
+    getStory(storyId).then(data => data && data.url && setStory(data));
+  }, []);
+
+  return story && story.url ? (
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+      <StoryMeta>
+        <span data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
+  
+      </StoryMeta>
+    </StoryWrapper>
+  ) : null;
+});
